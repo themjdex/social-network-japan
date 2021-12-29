f843d5fadc8fda633c38d5b4cc63bdadc9dea156
Marat Zhanpeisov <themjdex@gmail.com>
2021-12-29 07:39:17 +0500

    First commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+
+const Header = () => {
+    return (
+        <header className='header'>
+            <img src='https://w7.pngwing.com/pngs/542/197/png-transparent-prefectures-of-japan-map-japan-logo-map-world-map.png'></img>
+            <h1 className='service-name'>Student`s social network in Japan </h1>
+        </header>
+    )
+}
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,27 @@
+
+const Profile = () => {
+    return (
+        <div className='content'>
+            <div>
+                <img src='https://helpx.adobe.com/content/dam/help/en/photoshop/using/convert-color-image-black-white/jcr_content/main-pars/before_and_after/image-before/Landscape-Color.jpg'></img>
+            </div>
+            <div>
+                ava + description
+            </div>
+            <div>
+                my posts
+                <div>
+                    New post
+                </div>
+                <div>
+                    Post 1
+                </div>
+                <div>
+                    Post 2
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Profile;
