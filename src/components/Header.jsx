ec476420db2899e08c2d831966ff02795b1b3386
Marat Zhanpeisov <themjdex@gmail.com>
2021-12-29 16:30:36 +0500

    changes on css modules"

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,10 @@
+import s from './Header.module.css';
 
 const Header = () => {
     return (
-        <header className='header'>
+        <header className={s.header}>
             <img src='https://w7.pngwing.com/pngs/542/197/png-transparent-prefectures-of-japan-map-japan-logo-map-world-map.png'></img>
-            <h1 className='service-name'>Student`s social network in Japan </h1>
+            <h1 className={s.serviceName}>Student`s social network in Japan </h1>
         </header>
     )
 }
--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,7 +1,8 @@
+import s from './Profile.module.css';
 
 const Profile = () => {
     return (
-        <div className='content'>
+        <div className={s.content}>
             <div>
                 <img src='https://helpx.adobe.com/content/dam/help/en/photoshop/using/convert-color-image-black-white/jcr_content/main-pars/before_and_after/image-before/Landscape-Color.jpg'></img>
             </div>
@@ -13,11 +14,13 @@ const Profile = () => {
                 <div>
                     New post
                 </div>
-                <div>
-                    Post 1
-                </div>
-                <div>
-                    Post 2
+                <div className={s.posts}>
+                    <div className={s.item}>
+                        Post 1
+                    </div>
+                    <div className={s.item}>
+                        Post 2
+                    </div>
                 </div>
             </div>
         </div>
