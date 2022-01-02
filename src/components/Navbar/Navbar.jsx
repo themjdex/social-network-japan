2f10e765bedbb4d62210a0eacf562e374856efba
Marat Zhanpeisov <themjdex@gmail.com>
2022-01-02 10:26:19 +0500

    styling navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,22 +1,23 @@
+import { NavLink } from 'react-router-dom';
 import classes from './Navbar.module.css';
 
 const Navbar = () => {
     return (
         <nav className={classes.nav}>
-            <div className={`${classes.item} ${classes.active}`}>
-                <a href='/profile'>Profile</a>
+            <div className={classes.item}>
+                <NavLink to='/profile' className={navData => navData.isActive ? classes.active : classes.item}>Profile</NavLink>
             </div>
             <div className={classes.item}>
-                <a href='/dialogs'>Messages</a>
+                <NavLink to='/dialogs' className={navData => navData.isActive ? classes.active : classes.item}>Messages</NavLink>
             </div>
             <div className={classes.item}>
-                <a href='/news'>News</a>
+                <NavLink to='/news' className={navData => navData.isActive ? classes.active : classes.item}>News</NavLink>
             </div>
             <div className={classes.item}>
-                <a href='/groups'>Groups</a>
+                <NavLink to='/groups' className={navData => navData.isActive ? classes.active : classes.item}>Groups</NavLink>
             </div>
             <div className={classes.item}>
-                <a href='/settings'>Settings</a>
+                <NavLink to='/settings' className={navData => navData.isActive ? classes.active : classes.item}>Settings</NavLink>
             </div>
         </nav>
     )
