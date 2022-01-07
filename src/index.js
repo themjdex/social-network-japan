2ce390ae001a1001dfbc2987b30026b9a79702da
Marat Zhanpeisov <themjdex@gmail.com>
2022-01-07 11:59:49 +0500

    deleted render.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,19 @@
-
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import { addPost, subscribe, updateNewPostText } from './redux/state';
 import reportWebVitals from './reportWebVitals';
-import { rerenderEntireTree } from './render';
 import state from './redux/state';
 
-rerenderEntireTree(state);
+let rerenderEntireTree = (state) => {
+    ReactDOM.render(
+        <React.StrictMode>
+            <App state={state} addPost={addPost} updateNewPostText={updateNewPostText} />
+        </React.StrictMode>,
+        document.getElementById('root')
+    )
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+rerenderEntireTree(state);
 reportWebVitals();
+subscribe(rerenderEntireTree);
--- src/redux/state.js
+++ src/redux/state.js
@@ -1,4 +1,6 @@
-import { rerenderEntireTree } from '../render';
+let rerenderEntireTree = () => {
+    console.log('state was changed');
+}
 
 let state = {
     dialogsPage: {
@@ -30,7 +32,7 @@ let state = {
 
 window.state = state;
 
-export let addPost = () => {
+export const addPost = () => {
     let newPost = {
         id: 5,
         message: state.profilePage.newPostText,
@@ -38,12 +40,16 @@ export let addPost = () => {
     };
 
     state.profilePage.posts.push(newPost);
+    state.profilePage.newPostText = '';
     rerenderEntireTree(state);
 }
 
-export let updateNewPostText = (newText) => {
+export const updateNewPostText = (newText) => {
     state.profilePage.newPostText = newText;
     rerenderEntireTree(state);
+}
+export const subscribe = (observer) => {
+    rerenderEntireTree = observer;
 }
 
 export default state;
