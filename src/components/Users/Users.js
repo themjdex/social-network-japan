187c925ab24fbcfc67002da7a8ffdcf4b2941600
Marat Zhanpeisov <themjdex@gmail.com>
2022-01-26 10:56:23 +0500

    refacroting func component

---
+++ src/components/Users/Users.js
@@ -0,0 +1,45 @@
+import css from './users.module.css';
+import userPhoto from '../../assets/images/noimage.png';
+
+let Users = (props) => {
+    let pagesCount = Math.ceil((props.totalUsersCount / props.pageSize) / 10);
+
+    let pages = [];
+
+    for (let i = 1; i <= pagesCount; i++) {
+        pages.push(i);
+    };
+
+    return <div>
+        <div className={css.pagination}>
+            {pages.map(p => {
+                return <span className={props.currentPage === p ? css.currentPage : css.uncurrentPage} onClick={(e) => { props.onPageChanged(p) }}>{p}</span>
+            })}
+        </div>
+        {
+            props.users.map(u => <div key={u.id}>
+                <span>
+                    <div>
+                        <img src={u.photos.small != null ? u.photos.small : userPhoto} className={css.userPhoto} />
+                    </div>
+                    <div>
+                        {u.followed ? <button onClick={() => { props.unfollow(u.id) }}>Unfollow</button> : <button onClick={() => { props.follow(u.id) }}>Follow</button>}
+
+                    </div>
+                </span>
+                <span>
+                    <span>
+                        <div>{u.name}</div>
+                        <div>{u.status}</div>
+                    </span>
+                    <span>
+                        <div>{'u.location.country'}</div>
+                        <div>{'u.location.city'}</div>
+                    </span>
+                </span>
+            </div>)
+        }
+    </div>
+};
+
+export default Users;
